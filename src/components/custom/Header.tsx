95e06206e27b36c429593b3e71ca66d3bdccdc4f
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-13 23:01:04 +0530

    remove the unwanted imports

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -8,8 +8,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import axios from "@/utils/axios";
