ce86f1d2bed9a06d8a18c0cc77c413828824e2f0
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-19 05:50:59 +0530

    feat: added contact-form API

--- src/components/custom/ContactForm.tsx
+++ src/components/custom/ContactForm.tsx
@@ -1,13 +1,42 @@
 import { cn } from "@/lib/utils";
+import axios from "@/utils/axios";
 import { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ContactForm({ className }: { className: string }) {
-  const handleSubmit = (e: FormEvent) => {
+  const navigate = useNavigate();
+  const query = new URLSearchParams(window.location.search);
+  const success = query.get("success");
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const contact = {
+      data: {
+        fullName: formData.get("fullName"),
+        email: formData.get("email"),
+        subject: formData.get("subject"),
+        description: formData.get("description"),
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/contact-forms", contact);
+      if (res.status === 200) {
+        navigate("/contact-us?success=true");
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <div className={cn("p-6 rounded-lg bg-white", className)}>
+      {success && (
+        <div className="bg-green-100 text-green-600 p-2 rounded-md my-2">
+          Your message has been sent successfully
+        </div>
+      )}
       <h1 className="text-Black text-2xl font-bold font-open-sans md:text-4xl md:font-semibold">
         Contact us
       </h1>
@@ -23,7 +52,7 @@ export default function ContactForm({ className }: { className: string }) {
             id="fullName"
             type="text"
             required
-            className="w-full text-black border-b-2 border-gray-500 bg-transparent outline-none"
+            className="w-full text-black border-b-2 border-gray-500 bg-transparent outline-none p-1"
             name="fullName"
           />
         </div>
@@ -35,7 +64,7 @@ export default function ContactForm({ className }: { className: string }) {
             type="email"
             id="email"
             required
-            className="w-full text-Black border-b-2 border-gray-500 bg-transparent outline-none"
+            className="w-full text-Black border-b-2 border-gray-500 bg-transparent outline-none p-1"
             name="email"
           />
         </div>
@@ -47,7 +76,7 @@ export default function ContactForm({ className }: { className: string }) {
             type="text"
             id="subject"
             required
-            className="w-full text-Black border-b-2 border-gray-500 bg-transparent outline-none"
+            className="w-full text-Black border-b-2 border-gray-500 bg-transparent outline-none p-1"
             name="subject"
           />
         </div>
@@ -57,10 +86,10 @@ export default function ContactForm({ className }: { className: string }) {
           </label>
           <textarea
             cols={30}
-            rows={5}
+            rows={8}
             required
             name="description"
-            className="w-full mt-2 rounded-lg text-black border-2 border-gray-500 outline-none"
+            className="w-full mt-2 rounded-lg text-black border-2 border-gray-500 outline-none p-1"
           ></textarea>
         </div>
         <button
