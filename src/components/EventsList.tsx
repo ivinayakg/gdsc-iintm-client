7ef54b99de5fa86d082b4c167b927ab4abe9ad13
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-19 11:35:56 +0530

    events list, gap fix

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -2,7 +2,7 @@ import EventCard from "./custom/EventCard";
 
 export default function EventsList({ events }: { events: any[] }) {
   return (
-    <div className="flex flex-col justify-start items-center gap-4 mt-4 md:flex-row md:flex-wrap w-full p-2">
+    <div className="flex flex-col justify-start items-center gap-4 mt-4 md:flex-row md:flex-wrap w-full p-2 md:gap-6">
       {events &&
         events.map((event) => <EventCard key={event.id} event={event} />)}
     </div>
